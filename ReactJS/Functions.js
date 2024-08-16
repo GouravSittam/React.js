3f8ea9aa893cc467ccf23065e5c86c718ae2a772
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-16 11:21:28 +0530

    new

--- ReactJS/Functions.js
+++ ReactJS/Functions.js
@@ -5,9 +5,23 @@ function hello(){
 hello();
 
 //functions with loop parameters
-function printNum(n){
-    for(let i=0;i<n;i++);
-    console.log(i);
-}
-printNum(8);
+// function printNum(n){
+//     for(let i=0;i<n;i++);
+//     console.log(i);
+// }
+// printNum(8);
 
+//functions with multi parameters
+function add(a,b){
+    let sum=a+b;
+    console.log("The Sum is "+sum);
+}
+add(4,5);
+
+
+//function with return values and arguments
+function square(number){
+    return number*number;
+}
+const result = square(10);
+console.log(`Result: ${result}`);
