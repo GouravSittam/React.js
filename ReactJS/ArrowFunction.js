0f258083304d34db7e92ed687e06180b5ee47556
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-16 11:30:14 +0530

    new

--- ReactJS/ArrowFunction.js
+++ ReactJS/ArrowFunction.js
@@ -10,3 +10,7 @@ greet();
 //Arrow function with single input argument
 const s=x=>x*x;
 console.log(s(2));
+
+//Arrow function Single line function body
+const avi=(a,v)=>a+v;
+console.log(avi(1,8));
