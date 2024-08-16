4d73d5101cfdf603f3670dfab7cc21e75930b6e7
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-16 12:09:31 +0530

    new

---
+++ ReactJS/ThisKeyword.js
@@ -0,0 +1,11 @@
+console.log(this)
+
+//This key within the function
+function add(){
+    console.log(this)
+}
+add();
+
+//in strict mode this keyword will be undefined
+//non delete keyword,read only
+
