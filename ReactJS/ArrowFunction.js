db6e42855d1061f51474e1f38f135a66c7782f67
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-16 11:26:23 +0530

    new

--- ReactJS/ArrowFunction.js
+++ ReactJS/ArrowFunction.js
@@ -1,3 +1,12 @@
 //Arrow Function?
 console.log("Hello World");
 
+//Arrow function with no input argument
+const greet =()=>{
+    console.log("Hello world!");
+}
+greet();
+
+//Arrow function with single input argument
+const s=x=>x*x;
+console.log(s(2));
