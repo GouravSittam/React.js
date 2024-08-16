1d25f6951a9c1b34e2812ac4e551326395ab0b9a
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-16 11:44:52 +0530

    new

--- ReactJS/ArrowFunction.js
+++ ReactJS/ArrowFunction.js
@@ -14,3 +14,25 @@ console.log(s(2));
 //Arrow function Single line function body
 const avi=(a,v)=>a+v;
 console.log(avi(1,8));
+
+//Arrow function with single expression broken with multiple lines
+const mssg=(name,age)=>{
+    `Hello, ${name} you are ${age} year old`;
+}
+console.log(mssg('alice', '20'));
+
+
+//object liters return value
+const createPerson = ({name, age}) => ({
+    name: name,
+    age: age
+});
+
+const person = createPerson({name: "rahul", age: 25});
+console.log(person);
+
+//return key
+// return{
+//     name:name,
+//     age:age
+// }
