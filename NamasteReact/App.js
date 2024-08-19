6ad0198b9dd6d13c05c12f5a64c6ff87db6c75cc
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-19 18:21:59 +0530

    new data

--- NamasteReact/App.js
+++ NamasteReact/App.js
@@ -1,8 +1,25 @@
+//nested in react
 
-const heading= React.createElement(
+const parent = React.createElement("div",
+    { id: "parent" },
+    [ React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")
+    ])],
+    [ React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")
+    ])]
+);
+console.log(parent);
+
+
+const heading = React.createElement(
     "h1",
-    {id:"heading"},
-"Hello World from React!");
-console.log(heading)
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+    { id: "heading" },
+    "Hello World from React!");
+
+// console.log(heading);
+console.log(parent);//this react create element is returing objects
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+// root.render(heading);
