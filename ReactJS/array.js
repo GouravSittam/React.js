e60f1bda5d6193eaaa6cbcd3e3cefaa8f87646cd
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-21 14:49:02 +0530

    Array

---
+++ ReactJS/array.js
@@ -0,0 +1,8 @@
+const num=[1,2,3,2,3,3,4,33,5,6,6,7,88,8,7,6,5,55,6,7,7,7];
+//filter
+const evenNumbers=num.filter(num=>num%2==0);
+console.log(evenNumbers);
+
+const oddNumbers=num.filter(num=>num%2!=0);
+console.log(oddNumbers);
+
---
+++ ReactJS/class.js
@@ -0,0 +1,12 @@
+class stud{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    //method to show the student details
+    displayInfo(){
+        console.log("The name of the students is " + this.name);
+        console.log("The age of the students is " + this.age);
+    }
+}
+
