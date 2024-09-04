40a81a97a401f4d04d76fa8f1ce6f3bb7f2ad4ac
Gourav Chaudhary <gouravsittam@gmail.com>
2024-09-04 15:55:02 +0530

    Simple react page

--- ReactJS/reactjsx/src/App.js
+++ ReactJS/reactjsx/src/App.js
@@ -1,15 +1,56 @@
-// src/App.js
-import React from 'react';
-import Greeting from './Greeting';
-import Counter from './Counter';
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <Greeting message="Hello, Gourav! Welcome to the React App!" />
-      <Counter />
-    </div>
+function LpuSlide() {
+  return React.createElement(
+    "div",
+    {
+      style: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "white",
+      },
+    },
+    React.createElement(
+      "div",
+      {
+        style: {
+          backgroundColor: "#FDD835",
+          padding: "20px",
+          borderRadius: "8px",
+          textAlign: "center",
+          height:"150px",
+          border:"2px solid black"
+        },
+      },
+      React.createElement(
+        "h1",
+        {
+          style: {
+            color: "red",
+            fontSize: "3rem",
+            margin: 0,
+            padding:"5px"
+          },
+        },
+        "LPU"
+      ),
+      React.createElement(
+        "p",
+        {
+          style: {
+            color: "red",
+            fontSize: "1.5rem",
+            margin: 0,
+            marginTop: "10px",
+            // padding:"3px",
+          },
+        },
+        <b>Transforming Education, Transforming India</b>
+      )
+    )
   );
 }
 
-export default App;
+export default LpuSlide;
