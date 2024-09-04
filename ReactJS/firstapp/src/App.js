c74ab2cd0d81d806bf08fa84b7456f5cc64c3442
Gourav Chaudhary <gouravsittam@gmail.com>
2024-09-04 14:43:11 +0530

    Simple react page

--- ReactJS/firstapp/src/App.js
+++ ReactJS/firstapp/src/App.js
@@ -1,9 +1,36 @@
 // import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello React</h1>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="about.html">About</a>
+            </li>
+            <li>
+              <a href="service.html">Service</a>
+            </li>
+            <li>
+              <a href="contact.html">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>Welcome to my Website</h1>
+        <p>this is simple react page</p>
+      </main>
+
+      <footer>
+        <p>&copy; 2020 ReactJS</p>
+      </footer>
+    </div>
   );
-}
+};
 
 export default App;
