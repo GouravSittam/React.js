4ef9e77da3790e8f079a4fb971f0d1a1775fd337
Gourav Chaudhary <gouravsittam@gmail.com>
2024-09-20 20:32:14 +0530

    useContext, useState

--- ReactJS/vite-project/src/App.jsx
+++ ReactJS/vite-project/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ParentComponent from './Components/Parent';
-import './App.css';
+import React from "react";
+import ParentComponent from "./Components/Parent";
+// import AuthProvider from "./Components/AuthContext";
+// import AuthContext from "./Components/AuthContext"
+import LoginButton from "./Components/LoginButton"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <ParentComponent />
+      {/* <AuthContext /> */}
+      {/* <AuthProvider /> */}
+      <LoginButton />
     </div>
   );
 }
--- ReactJS/vite-project/src/Components/LoginButton.jsx
+++ ReactJS/vite-project/src/Components/LoginButton.jsx
@@ -1,5 +1,18 @@
-const loginBtn = () => {
-    return <button>Login</button>;
+import React, { useContext } from "react";
+import { AuthContext } from "./AuthContext";
+
+const LoginButton = () => {
+  const { isAuthenticated, login, logout } = useContext(AuthContext);
+
+  return (
+    <div>
+      {isAuthenticated ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <button onClick={login}>Login</button>
+      )}
+    </div>
+  );
 };
 
-export default loginBtn;
+export default LoginButton;
